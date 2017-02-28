bc5a8d74e1cbe54231404a8e1907352ee7c91119
Geert Bevin <gbevin@uwyn.com>
2017-02-28 06:11:06 +0100

    Updated version to 1.0.3

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -32,8 +32,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "receivemidi";
-    const char* const  versionString  = "1.0.2";
-    const int          versionNumber  = 0x10002;
+    const char* const  versionString  = "1.0.3";
+    const int          versionNumber  = 0x10003;
 }
 #endif
 
