68c75f323f14299ce618081520c4e421bac3c947
Geert Bevin <gbevin@uwyn.com>
2019-01-11 10:24:38 -0500

    Updated copyright year

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,6 +1,6 @@
 /*
  * This file is part of ReceiveMIDI.
- * Copyright (command) 2017 Uwyn SPRL.  http://www.uwyn.com
+ * Copyright (command) 2017-2019 Uwyn SPRL.  http://www.uwyn.com
  *
  * ReceiveMIDI is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
