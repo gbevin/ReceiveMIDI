57b8b57359b1bf516e6097e131282e4712f9449d
Geert Bevin <gbevin@uwyn.com>
2024-10-15 20:46:51 -0400

    Fix macOS build script to build for all architectures with newer Xcode

--- Scripts/build-macos.sh
+++ Scripts/build-macos.sh
@@ -21,7 +21,7 @@ mkdir -v "$INSTALLERS_DIR"
 
 # build release artifacts
 echo "Building all ProJucer artifacts"
-xcodebuild -project ./Builds/MacOSX/receivemidi.xcodeproj -config Release SYMROOT=build -scheme "receivemidi - ConsoleApp"
+xcodebuild -project ./Builds/MacOSX/receivemidi.xcodeproj -destination "generic/platform=macOS,name=Any Mac" -config Release SYMROOT=build -scheme "receivemidi - ConsoleApp"
 
 # sign each individual artifact
 echo "Codesigning all artifacts"
